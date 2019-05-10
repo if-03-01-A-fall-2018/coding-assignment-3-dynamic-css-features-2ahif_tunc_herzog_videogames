176a14a9a157e01ae330b9195afe64b6ae3dfe5f
NathalieHerzog <nathalieherzog2003@gmail.com>
2019-05-10 13:43:17 +0200

    no errors but games aren't displaying?

--- Website_Videogames/tetris.js
+++ Website_Videogames/tetris.js
@@ -1,11 +1,9 @@
 function get(id)        { return document.getElementById(id);  }
     function hide(id)       { get(id).style.visibility = 'hidden'; }
-    function show(id)       { get(id).style.visibility = null;     }
-    function html(id, html) { get(id).innerHTML = html;            }
     function timestamp()           { return new Date().getTime();                             }
     function random(min, max)      { return (min + (Math.random() * (max - min)));            }
     function randomChoice(choices) { return choices[Math.round(random(0, choices.length-1))]; }
-    if (!window.requestAnimationFrame) { // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+    if (!window.requestAnimationFrame) {
       window.requestAnimationFrame = window.webkitRequestAnimationFrame ||
                                      window.mozRequestAnimationFrame    ||
                                      window.oRequestAnimationFrame      ||
@@ -21,8 +19,6 @@ function get(id)        { return document.getElementById(id);  }
         DIR     = { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3, MIN: 0, MAX: 3 },
         canvas  = get('canvas'),
         ctx     = canvas.getContext('2d'),
-        ucanvas = get('upcoming'),
-        uctx    = ucanvas.getContext('2d'),
         speed   = { start: 0.6, decrement: 0.005, min: 0.1 }, // how long before piece drops by 1 row (seconds)
         nx      = 10, // width of tetris court (in blocks)
         ny      = 20, // height of tetris court (in blocks)
@@ -107,24 +103,18 @@ function get(id)        { return document.getElementById(id);  }
     // GAME LOOP
     //-------------------------------------------------------------------------
     function run() {
-      showStats(); // initialize FPS counter
       addEvents(); // attach keydown and resize events
       var last = now = timestamp();
       function frame() {
         now = timestamp();
         update(Math.min(1, (now - last) / 1000.0)); // using requestAnimationFrame have to be able to handle large delta's caused when it 'hibernates' in a background or non-visible tab
         draw();
-        stats.update();
         last = now;
         requestAnimationFrame(frame, canvas);
       }
       resize(); // setup all our sizing information
       reset();  // reset the per-game variables
       frame();  // start the first frame
-    }
-    function showStats() {
-      stats.domElement.id = 'stats';
-      get('menu').appendChild(stats.domElement);
     }
     function addEvents() {
       document.addEventListener('keydown', keydown, false);
@@ -133,8 +123,6 @@ function get(id)        { return document.getElementById(id);  }
     function resize(event) {
       canvas.width   = canvas.clientWidth;  // set canvas logical size equal to its physical size
       canvas.height  = canvas.clientHeight; // (ditto)
-      ucanvas.width  = ucanvas.clientWidth;
-      ucanvas.height = ucanvas.clientHeight;
       dx = canvas.width  / nx; // pixel size of a single tetris block
       dy = canvas.height / ny; // (ditto)
       invalidate();
@@ -162,7 +150,7 @@ function get(id)        { return document.getElementById(id);  }
     // GAME LOGIC
     //-------------------------------------------------------------------------
     function play() { hide('start'); reset();          playing = true;  }
-    function lose() { show('start'); setVisualScore(); playing = false; }
+    function lose() { playing = false; }
     function setVisualScore(n)      { vscore = n || score; invalidateScore(); }
     function setScore(n)            { score = n; setVisualScore(n);  }
     function addScore(n)            { score = score + n;   }
@@ -287,7 +275,6 @@ function get(id)        { return document.getElementById(id);  }
       ctx.translate(0.5, 0.5); // for crisp 1px black lines
       drawCourt();
       drawNext();
-      drawScore();
       drawRows();
       ctx.restore();
     }
@@ -309,26 +296,19 @@ function get(id)        { return document.getElementById(id);  }
     }
     function drawNext() {
       if (invalid.next) {
-        var padding = (nu - next.type.size) / 2; // half-arsed attempt at centering next piece display
-        uctx.save();
-        uctx.translate(0.5, 0.5);
-        uctx.clearRect(0, 0, nu*dx, nu*dy);
-        drawPiece(uctx, next.type, padding, padding, next.dir);
-        uctx.strokeStyle = 'black';
-        uctx.strokeRect(0, 0, nu*dx - 1, nu*dy - 1);
-        uctx.restore();
+        var padding = (nu - next.type.size) / 2;
+        ctx.save();
+        ctx.translate(0.5, 0.5);
+        ctx.clearRect(0, 0, nu*dx, nu*dy);
+        drawPiece(ctx, next.type, padding, padding, next.dir);
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(0, 0, nu*dx - 1, nu*dy - 1);
+        ctx.restore();
         invalid.next = false;
-      }
-    }
-    function drawScore() {
-      if (invalid.score) {
-        html('score', ("00000" + Math.floor(vscore)).slice(-5));
-        invalid.score = false;
       }
     }
     function drawRows() {
       if (invalid.rows) {
-        html('rows', rows);
         invalid.rows = false;
       }
     }
