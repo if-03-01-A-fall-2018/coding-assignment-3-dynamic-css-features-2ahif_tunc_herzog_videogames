7cc9c09fac539760e8bd43eb437777e674ef7470
Herzog Nathalie <if170048@WS8A14.local>
2019-05-27 13:35:53 +0200

    working on buttons

---
+++ Website_Videogames/playstyleChooser.js
@@ -0,0 +1,10 @@
+function playstyleChooser(style) {
+    if(style == 1)
+    {
+          document.getElementById(style).innerHTML = pongTESTING_PVP.js;
+    }
+    else
+    {
+        document.getElementById(style).innerHTML = pong.js;
+    }
+}
