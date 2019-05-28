18722be07fa328a2ba218ad6b099be8f07ecc481
Herzog Nathalie <if170048@WS8A03.local>
2019-05-28 17:54:50 +0200

    updated todo

--- Website_Videogames/snake.js
+++ Website_Videogames/snake.js
@@ -3,18 +3,18 @@ var context = canvas.getContext('2d');
 var grid = 16;
 var count = 0;
 var score = 0;
-  
+
 var snake = {
   x: 160,
   y: 160,
-  
+
   // snake velocity. moves one grid length every frame in either the x or y direction
   dx: grid,
   dy: 0,
-  
+
   // keep track of all grids the snake body occupies
   cells: [],
-  
+
   // length of the snake. grows when eating a red block
   maxCells: 4
 };
@@ -54,7 +54,7 @@ function loop() {
   else if (snake.x >= canvas.width) {
     snake.x = 0;
   }
-  
+
   // wrap snake position vertically on edge of screen
   if (snake.y < 0) {
     snake.y = canvas.height - grid;
@@ -68,7 +68,7 @@ function loop() {
   if (snake.cells.length > snake.maxCells) {
     snake.cells.pop();
   }
-  //draw scoer
+  //draw score
   context.font = "30px Arial";
   context.fillStyle = 'white';
   context.fillText(score.text, score.x, score.y);
@@ -80,21 +80,21 @@ function loop() {
   snake.cells.forEach(function(cell, index)
   {
     // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
-    context.fillRect(cell.x, cell.y, grid-1, grid-1);  
+    context.fillRect(cell.x, cell.y, grid-1, grid-1);
     // snake ate red block
     if (cell.x === apple.x && cell.y === apple.y) {
       snake.maxCells++;
       score.temp++;
-      
+
       score.text = "SCORE: " + score.temp;
-      
-      // canvas is 400x400 which is 25x25 grids 
+
+      // canvas is 400x400 which is 25x25 grids
       apple.x = getRandomInt(0, 25) * grid;
       apple.y = getRandomInt(0, 25) * grid;
     }
     // check collision with all cells after this one (modified bubble sort)
     for (var i = index + 1; i < snake.cells.length; i++) {
-      
+
       // snake occupies same space as a body part. reset game
       if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
         snake.x = 160;
@@ -113,11 +113,11 @@ function loop() {
 
 // listen to keyboard events to move the snake
 document.addEventListener('keydown', function(e) {
-  // prevent snake from backtracking on itself by checking that it's 
+  // prevent snake from backtracking on itself by checking that it's
   // not already moving on the same axis (pressing left while moving
   // left won't do anything, and pressing right while moving left
   // shouldn't let you collide with your own body)
-  
+
   // left arrow key
   if (e.which === 37 && snake.dx === 0) {
     snake.dx = -grid;
