7509836057b40aa096d2d6f5a24a2aa068ce4eee
Herzog Nathalie <if170048@WS8A03.local>
2019-05-28 17:08:05 +0200

    - Introduced class game-area - Removed script from playstyleChooser.js

--- Website_Videogames/playstyleChooser.js
+++ Website_Videogames/playstyleChooser.js
@@ -1,7 +1,4 @@
 function playstyleChooser(mode) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript'; 
-
     if(mode == 1)
     {
         document.getElementById('AI').style.display = "none";
