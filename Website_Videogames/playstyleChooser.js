40e38ca832e622a295c92696116531d35b1c85e6
Herzog Nathalie <if170048@EDU.HTL-LEONDING.AC.AT>
2019-06-06 14:28:27 +0200

    -fixed calling pong

--- Website_Videogames/playstyleChooser.js
+++ Website_Videogames/playstyleChooser.js
@@ -1,7 +1,12 @@
-window.onload = function() {
-    document.querySelector("#PVP-button").onclick = playstyleChooser(0)
-    document.querySelector("#AI-button").onclick = playstyleChooser(1)
-}
+document.querySelector("#PVP-button").addEventListener("click", function() 
+{
+    playstyleChooser(1);
+});
+
+document.querySelector("#AI-button").addEventListener("click", function() 
+{
+    playstyleChooser(0);
+});
 
 function playstyleChooser(mode) {
     if(mode == 1)
@@ -9,7 +14,7 @@ function playstyleChooser(mode) {
         document.getElementById('AI').style.display = "none";
         document.getElementById('PVP').style.display = "block";
     }
-    else if(mode == 0)
+    else if(mode === 0)
     {
         document.getElementById('PVP').style.display = "none";
         document.getElementById('AI').style.display = "block";
